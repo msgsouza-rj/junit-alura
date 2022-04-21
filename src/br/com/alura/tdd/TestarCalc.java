ed9f1c9091814a5be2aa7ff4b2505b766328d2a1
msgsouza-rj <msgsouza.rj@gmail.com>
2022-04-21 18:57:29 -0300

    furst commit

---
+++ src/br/com/alura/tdd/TestarCalc.java
@@ -0,0 +1,15 @@
+package br.com.alura.tdd;
+
+public class TestarCalc {
+
+	public static void main(String[] args) {
+		
+		Calculadora calc = new Calculadora();
+		
+//		calc.somar(2, 2);
+		
+		System.out.println(calc.somar(2, 2));
+
+	}
+
+}
